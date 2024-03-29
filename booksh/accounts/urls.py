8aaf7e924a49b710712d3d7114161b6b9fd2a26b
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2024-03-29 12:51:11 +0200

    3 commit

--- booksh/accounts/urls.py
+++ booksh/accounts/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
 
+from booksh.accounts.views import SignUpUserView, SignInUserView
+
+urlpatterns = [
+    path("signup/", SignUpUserView.as_view(), name="signup user"),
+    path("signin/", SignInUserView.as_view(), name="signin user"),
 ]
--- booksh/accounts/views.py
+++ booksh/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import AccessMixin
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import views as auth_views, login, logout
+from django.urls import reverse_lazy, reverse
+from django.views import generic as views
+
+from booksh.accounts.forms import BookshUserCreationForm
+from booksh.accounts.models import BookshUser, Profile
+
+
+class OwnerRequiredMixin(AccessMixin):
+    """Verify that the current user has this profile."""
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.pk != kwargs.get('pk', None):
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class SignInUserView(auth_views.LoginView):
+    template_name = "accounts/signin_user.html"
+    redirect_authenticated_user = True
+
+
+class SignUpUserView(views.CreateView):
+    template_name = "accounts/signup_user.html"
+    form_class = BookshUserCreationForm
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        # `form_valid` will call `save`
+        result = super().form_valid(form)
+
+        login(self.request, form.instance)
+
+        return result
+
+
+def signout_user(request):
+    logout(request)
+    return redirect('index')
+
